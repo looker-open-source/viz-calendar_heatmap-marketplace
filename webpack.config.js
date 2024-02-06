483ecad2ca5a268cc6d738bc995c122930fe3c18
Andres Rubio <rubioandres@google.com>
2024-02-06 14:15:26 +0000

    fix: bundle name

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ var webpackConfig = {
     contentBase: "./dist",
   },
   output: {
-    filename: "calendar_chart.js",
+    filename: "bundle.js",
     path: path.join(path.resolve(__dirname), "/dist"),
     library: "[name]",
     libraryTarget: "umd",
